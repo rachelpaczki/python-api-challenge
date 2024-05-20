2470bb11a425d650d308fb349845bd9fa0ae82b4
rachelpaczki <rachelpuzycki@gmail.com>
2024-05-20 18:53:31 -0500

    update

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "e54a45bbc1050ed4a684f2297ccafe9e"
+weather_api_key = "f058abb6011784f3c807b3bda076f658"
 
 # Geoapify API Key
 geoapify_key = "ef8ecd57530b4973a0249c42727e1f35"
